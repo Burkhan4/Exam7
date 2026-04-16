fca7c2ccc450abbec66dbb61c9970738ef353e18
Burkhan4 <alexandrey670@gmail.com>
2026-04-16 13:34:44 +0500

    1.6

--- src/assets/config/axios.config.js
+++ src/assets/config/axios.config.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
-const backendBaseUrl = import.meta.env.DEV
-  ? "/api"
-  : "https://e-commerce-api-v4.nt.azimumarov.uz";
-
 export default axios.create({
-    baseURL: backendBaseUrl,
-    headers: {
-        "Accept": "application/json",
-    },
+  baseURL: "/api",
+  headers: {
+    "Accept": "application/json",
+  },
 });
